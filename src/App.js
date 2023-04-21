653e2aefc0108fe5562fd30f77a443a1801a63aa
김성한 <k0s0a7@naver.com>
2023-04-21 13:56:44 +0900

    landing Page

--- src/App.js
+++ src/App.js
@@ -1,22 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./assets/Base_Cobby_Final.gif";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h2>COBBY - Github with COBBY</h2>
+
+        <h1>Coming Soon</h1>
       </header>
     </div>
   );
